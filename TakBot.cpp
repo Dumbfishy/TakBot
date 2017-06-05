b385d187d71ad66cb9bf3dab493b7b8c12ed9fc1
Kyle Johnston <kjohn402@ford.com>
2017-06-05 13:02:15 -0400

    Initial Tak Commit

---
+++ TakBot.cpp
@@ -0,0 +1,29 @@
+/***************************
+ *
+ *
+ *  TakBot.cpp
+ *  Kyle Johnston
+ *
+ *
+ * ************************/
+
+#include <iostream>
+
+void showBoard();
+
+int main()
+{
+    
+    std::cout << "Hello TakBot!" << std::endl;
+
+    showBoard();
+
+    return 0;
+
+}
+
+
+void showBoard()
+{
+    std::cout << "Getting ready to display board" << std::endl;
+}
